1791c1d2c47b78f20f66e53d8097cc53bbf668e4
saurabh110398 <saurabh110398@gmail.com>
2024-10-03 19:37:24 +0530

    Appointment component

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
+import { assets } from '../assets/assets';
 
 const Appointment = () => {
-  return (
-    <div>
-      appoinments
+
+  const { docId } = useParams();
+  let { doctors, currencySymbol } = useContext(AppContext);
+  
+  let [docInfo, setDocInfo] = useState({});
+
+  const fetchDocInfo = async () => {
+    let presentDocInfo = doctors.find(ele => ele._id == docId);
+    setDocInfo(presentDocInfo);
+    console.log('presentDocInfo:: ', presentDocInfo);
+
+  }
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [docId, doctors])
+
+
+  return docInfo && (
+
+    <div className='gap-4'>
+      <div className='flex flex-col md:flex-row  gap-4   w-full'>
+        <div className='bg-primary  rounded-lg w-2/3 md:w-1/4 '>
+          <img className='bottom-0' src={docInfo?.image} alt="" />
+        </div>
+
+        <div className=' border-2 rounded-lg w-2/3 lg:w-full space-y-2 p-3'>
+          <p className=' flex gap-2 text-3xl md:text-4xl font-medium'>{docInfo.name}
+            <img src={assets.verified_icon} alt="" />
+          </p>
+          <div className='flex text-md md:text-xl text-gray-500 gap-2'>
+            <p>{docInfo.degree} - {docInfo.speciality}</p>
+
+            <button className='border rounded-xl text-sm md:text-lg px-2 hidden lg:flex'>{docInfo.experience}</button>
+          </div>
+          <p className='border rounded-xl text-sm md:text-lg px-2 inline lg:hidden'>{docInfo.experience}</p>
+
+          <div>
+            <p className='text-sm md:text-lg flex gap-2 '>About <img className='w-3' src={assets.info_icon} alt="" /></p>
+            <p className='text-sm md:text-lg text-gray-500'>{docInfo.about}</p>
+          </div>
+          <p className='text-md md:text-lg'>Appointment Fee: {currencySymbol}{docInfo.fees}</p></div>
+      </div>
+      <div className=' '>2</div>
+      <div className='border-2 '>3</div>
     </div>
   )
 }
--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -10,17 +10,17 @@ const Doctors = () => {
 
   let [filterDoc, setFilterDoc] = useState([]);
 
-  const applyFilter =()=>{
-    if(speciality){
-      setFilterDoc(doctors.filter(ele=> ele.speciality === speciality))
+  const applyFilter = () => {
+    if (speciality) {
+      setFilterDoc(doctors.filter(ele => ele.speciality === speciality))
     }
-    else{
+    else {
       setFilterDoc(doctors)
     }
   }
-  useEffect(()=>{
-applyFilter()
-  },[doctors, speciality])
+  useEffect(() => {
+    applyFilter()
+  }, [doctors, speciality])
 
 
   return (
@@ -37,20 +37,22 @@ applyFilter()
               )
             })
           } */}
-          <button onClick={()=>{navigate(`/doctors/${speciality}`)}} className='px-3 py-2 border rounded-md '>Gynecologist</button>
-          <button onClick={()=>{navigate(`/doctors/${speciality}`)}} className='px-3 py-2 border rounded-md'>Dermatologist</button>
-          <button onClick={()=>{navigate(`/doctors/${speciality}`)}} className='px-3 py-2 border rounded-md'>Pediatricians</button>
-          <button onClick={()=>{navigate(`/doctors/${speciality}`)}} className='px-4 py-2 border rounded-md '>General Physician</button>
-          <button onClick={()=>{navigate(`/doctors/${speciality}`)}} className='px-3 py-2 border rounded-md'>Neurologist</button>
-          <button onClick={()=>{navigate(`/doctors/${speciality}`)}} className='px-3 py-2 border rounded-md'>Gastroenterologist</button>
+          <button onClick={() =>  { speciality === 'Gynecologist'? navigate('/doctors'): navigate('/doctors/Gynecologist') }} className= {`px-3 py-2 border rounded-md ${speciality === 'Gynecologist' ? 'bg-indigo-100 text-black' : ''}`} >Gynecologist</button>
+          <button onClick={() =>  { speciality === 'Dermatologist'?navigate('/doctors'): navigate('/doctors/Dermatologist') }} className= {`px-3 py-2 border rounded-md ${speciality === 'Dermatologist' ? 'bg-indigo-100 text-black' : ''}`}>Dermatologist</button>
+          <button onClick={() =>  { speciality === 'Pediatricians'?navigate('/doctors'): navigate(`/doctors/Pediatricians`) }} className= {`px-3 py-2 border rounded-md ${speciality === 'Pediatricians' ? 'bg-indigo-100 text-black' : ''}`}>Pediatricians</button>
+          <button onClick={() =>  { speciality === 'General physician'?navigate('/doctors'): navigate(`/doctors/General physician`) }} className= {`px-3 py-2 border rounded-md ${speciality === 'General physician' ? 'bg-indigo-100 text-black' : ''}`}>General Physician</button>
+          <button onClick={() =>  { speciality === 'Neurologist'?navigate('/doctors'): navigate(`/doctors/Neurologist`) }} className= {`px-3 py-2 border rounded-md ${speciality === 'Neurologist' ? 'bg-indigo-100 text-black' : ''}`}>Neurologist</button>
+          <button onClick={() =>  { speciality === 'Gastroenterologist'?navigate('/doctors'): navigate(`/doctors/Gastroenterologist`) }} className= {`px-3 py-2 border rounded-md ${speciality === 'Gastroenterologist' ? 'bg-indigo-100 text-black' : ''}`}>Gastroenterologist</button>
         </div>
-       
+
         {/* docs */}
-        <div className='flex flex-col md:flex-row flex-wrap justify-center items-center gap-5'>
+        <div className=' border-3 border-yellow-400 flex flex-col md:flex-row flex-wrap justify-center items-center gap-5'>
           {
             filterDoc.map(ele => {
               return (
-                <div className=' border-2 rounded-xl md:w-[220px] flex flex-col' key={ele._id}>
+                <div className=' border-2 rounded-xl md:w-[220px] flex flex-col cursor-pointer overflow-hidden hover:translate-y-[-10px] transition-all duration-500'
+                  key={ele._id}
+                  onClick={() => { navigate(`/appointment/${ele._id}`) }}>
                   <img className='bg-blue-50' src={ele.image} alt="" />
                   <div className='p-5 space-y-2'>
                     <div className='flex items-center justify-start gap-2'>
